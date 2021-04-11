b23499889f71c9c2d7ae6a10e2fdbfad71245465
Union Hills Software <unionhills@cox.net>
2021-04-10 17:59:49 -0700

    Added NoteController.  Removed any reference to the WeatherForecast functionality.

---
+++ RubeGoldbergFunction/Api/Controllers/NotesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using RubeGoldbergFunction.Core.Entities;
+using RubeGoldbergFunction.Core.Services;
+
+namespace RubeGoldbergFunction.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly ILogger<NotesController> _logger;
+        private readonly NoteService _noteService;
+
+        public NotesController(ILogger<NotesController> logger)
+        {
+            _logger = logger;
+            _noteService = new();
+        }
+
+        [HttpGet]
+        public IEnumerable<Note> Get()
+        {
+            ICollection<Note> notes = _noteService.GetNotes();
+
+            return (notes);
+        }
+    }
+}
